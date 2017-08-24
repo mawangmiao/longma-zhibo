27b8dfad2e157e9bc1537be15069c04b97f2ff34
彭爽 <pengshuang@oneapm.com>
2017-08-24 21:13:31 +0800

    实现服务注册和账户中心

---
+++ longma-site/longma-account-service/src/main/java/com/pengshuang/longma/account_service/controller/AccountController.java
@@ -0,0 +1,26 @@
+package com.pengshuang.longma.account_service.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class AccountController {
+    private final DiscoveryClient client;
+
+    public AccountController(DiscoveryClient client) {this.client = client;}
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        log.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
+    }
+
+}

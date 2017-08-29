d3d7a62798dcbeca83d43429a138786276a93641
彭爽 <pengshuang@oneapm.com>
2017-08-29 18:08:12 +0800

    no message

--- longma-site/chat-message-service/src/main/java/com/pengshuang/longma/service/chat_message/controller/ChatMessageController.java
+++ longma-site/chat-message-service/src/main/java/com/pengshuang/longma/service/chat_message/controller/ChatMessageController.java
@@ -1,11 +1,13 @@
 package com.pengshuang.longma.service.chat_message.controller;
 
-import com.pengshuang.longma.common.entity.StringResult;
 import com.pengshuang.longma.api.message.ChatMessage;
 import com.pengshuang.longma.api.message.ChatMessageService;
+import com.pengshuang.longma.common.entity.StringResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class ChatMessageController implements ChatMessageService {
@@ -16,8 +18,8 @@ public class ChatMessageController implements ChatMessageService {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String welcome() {
-        return "Welcome to Chat Service";
+    public String welcome(Principal user) {
+        return "Welcome to Chat Service, " + user.getName();
     }
 
 }

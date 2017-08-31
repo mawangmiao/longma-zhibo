1480cb91dd2c36aad317284714e3821d17f84aaa
彭爽 <pengshuang@oneapm.com>
2017-08-31 14:06:33 +0800

    no message

--- longma-site/chat-message-service/src/main/java/com/pengshuang/longma/service/chat_message/controller/ChatMessageController.java
+++ longma-site/chat-message-service/src/main/java/com/pengshuang/longma/service/chat_message/controller/ChatMessageController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.security.Principal;
 
 @RestController
@@ -19,7 +21,18 @@ public class ChatMessageController implements ChatMessageService {
 
     @RequestMapping(value = "/welcome", method = RequestMethod.GET)
     public String welcome(Principal user) {
-        return "Welcome to Chat Service, " + user.getName();
+        String ip = null;
+        String host = null;
+
+        try {
+            InetAddress ia = InetAddress.getLocalHost();
+            host = ia.getHostName();//获取计算机主机名
+            ip = ia.getHostAddress();//获取计算机IP
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        return String.format("[%s] [%s] Welcome to Chat Service, %s", ip, host, user.getName());
     }
 
 }
--- longma-site/docker/start.sh
+++ longma-site/docker/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker-compose -p longma up -d --build
+echo "扩容..."
+docker-compose -p longma scale chat-message-service=3

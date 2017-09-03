5f36d2b70c4aeaa371f37f82d4ac3ee10a332ce1
彭爽 <pengshuang@oneapm.com>
2017-09-03 18:49:18 +0800

    no message

---
+++ longma-site/gateway/src/test/java/com/pengshuang/longma/gateway/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.pengshuang.longma.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = {"eureka.client.enabled=false"})
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}

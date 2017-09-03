8b83b0eda037e67f0c7a46c6cc5d8576f95e6613
彭爽 <pengshuang@oneapm.com>
2017-09-03 16:16:09 +0800

    no message

--- longma-site/docker/container.sh
+++ longma-site/docker/container.sh
@@ -6,7 +6,7 @@ function wait_container_ok_by_log {
   COUNT=0
   while [[ -z "$STARTED" && COUNT -lt $1 ]]; do
     STARTED=$(docker-compose logs $2 2>&1 | grep "$3")
-    echo "waiting startup of $2"
+    echo "等待$2启动"
     sleep 1
     COUNT=$(($COUNT+1))
   done
@@ -14,7 +14,9 @@ function wait_container_ok_by_log {
   if [[ -z "$STARTED" ]]; then
     #启动失败
     echo "timeout waiting startup of $2"
-    docker-compose logs -t
+    echo -e "\033[31m $2启动超时 \033[0m"
     exit 1
   fi
+
+  echo -e "\033[32m $2启动完毕 \033[0m"
 }
--- longma-site/docker/start.sh
+++ longma-site/docker/start.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-docker-compose up -d --build
-echo "扩容..."
+SECONDS=0
+WAIT_TIMEOUT=120
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $BASE_DIR/container.sh
+
+echo "启动ELK和Zipkin"
+docker-compose up -d elasticsearch logstash kibana zipkin
+wait_container_ok_by_log $WAIT_TIMEOUT "elasticsearch" "started"
+wait_container_ok_by_log $WAIT_TIMEOUT "logstash" "started Logstash"
+wait_container_ok_by_log $WAIT_TIMEOUT "zipkin" "Started ZipkinServer"
+
+echo "启动服务注册中心"
+docker-compose up -d service-register-center
+wait_container_ok_by_log $WAIT_TIMEOUT "service-register-center" "Started Application"
+
+echo "启动各种服务"
+docker-compose up -d
 docker-compose scale chat-message-service=2
+wait_container_ok_by_log $WAIT_TIMEOUT "gateway" "Started Application"
+wait_container_ok_by_log $WAIT_TIMEOUT "auth-service" "Started Application"
+wait_container_ok_by_log $WAIT_TIMEOUT "chat-message-service" "Started Application"
+
+wait_container_ok_by_log $WAIT_TIMEOUT "kibana" "Server running at"
+
+DURATION=$SECONDS
+echo -e "\033[32m 启动完毕, 耗时$(($DURATION / 60))分$(($DURATION % 60))秒 \033[0m"
